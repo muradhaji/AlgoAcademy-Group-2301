0533e9c06d1f83543cba5dd40ac1cfe0710e87cd
muradhaji <muradhajiyev42@icloud.com>
2024-02-17 15:18:21 +0400

    loops explained

---
+++ JavaScript/JS 7/js/loops.js
@@ -0,0 +1,47 @@
+// let num = Number(prompt("Enter num:"));
+// let sum = 0;
+// let n = 1;
+
+// while(num > 0) {
+// 	console.log(num);
+// 	num -= 4;
+// 	sum += num--;
+// }
+
+// while(n <= num) {
+// 	sum += n++;
+// }
+
+// console.log(sum);
+// console.log(num);
+// console.log(n);
+
+// let i = 3;
+
+// while(i > 0) {
+// 	console.log(i);
+// 	i--;
+// };
+
+// do {
+// 	i--;
+// 	console.log(i);
+	
+// } while(i > 0);
+
+// for(let i = 3 ; i < 1000 ; i *= 4) {
+// 	console.log(`Iteration ${i}`)
+// }
+
+let i = 3, x = 2 ;
+
+for( ; i > 0 ; ) {
+	console.log(`Iteration ${i} ${x} ${i + x}`)
+	 i--;
+	 x++;
+}
+
+console.log(i);
+
+alert(y + " - " + z);
+alert(`${x} - ${y}`);
