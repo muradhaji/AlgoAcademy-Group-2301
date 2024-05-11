570066abc7753efab65d4a06008310ae2a4575ee
muradhaji <muradhajiyev42@icloud.com>
2024-05-12 01:26:03 +0400

    Post details finished

--- React/postapp/src/PostDetails.js
+++ React/postapp/src/PostDetails.js
@@ -1,10 +1,18 @@
 import { isEmpty } from 'lodash';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import './PostDetails.css';
 
 function PostDetails() {
   let [post, setPost] = useState(null);
   let [postLoading, setPostLoading] = useState(false);
+
+  let [user, setUser] = useState(null);
+  let [userLoading, setUserLoading] = useState(false);
+
+  let [comments, setComments] = useState([]);
+  let [commentsLoading, setCommentsLoading] = useState(false);
+
   let params = useParams();
   let { postId } = params;
 
@@ -26,6 +34,54 @@ function PostDetails() {
       });
   }, []);
 
+  useEffect(() => {
+    if (post) {
+      setUserLoading(true);
+      fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`, {
+        method: 'GET',
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          if (!isEmpty(data)) {
+            setUser(data);
+          }
+          setUserLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setUserLoading(false);
+        });
+    }
+  }, [post]);
+
+  useEffect(() => {
+    if (post) {
+      setCommentsLoading(true);
+      fetch(`https://jsonplaceholder.typicode.com/comments`, {
+        method: 'GET',
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          let newArr = [];
+          for (let i = 0; i < data.length; i++) {
+            if (data[i].postId === post.id) {
+              newArr.push(data[i]);
+            }
+          }
+          setComments(newArr);
+          setCommentsLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setCommentsLoading(false);
+        });
+    }
+  }, [post]);
+
   return (
     <div className='postDetails'>
       {postLoading ? (
@@ -33,7 +89,15 @@ function PostDetails() {
       ) : post ? (
         <div>
           <div>
-            <b>User:</b>
+            {userLoading ? (
+              'User loading ...'
+            ) : user ? (
+              <>
+                <b>User:</b> <Link to={`/users/${user.id}`}>{user.name}</Link>
+              </>
+            ) : (
+              'User not found.'
+            )}
           </div>
           <div>
             <div>
@@ -43,8 +107,22 @@ function PostDetails() {
               <b>Body:</b> {post.body}
             </div>
           </div>
-          <div>
-            <b>Comments:</b>
+          <div className='commentsHolder'>
+            <div>
+              <b>Comments:</b>
+            </div>
+            {commentsLoading
+              ? 'Comments loading ...'
+              : comments.length
+              ? comments.map((comment) => (
+                  <div className='comment'>
+                    <div>
+                      <b>{comment.email}</b>
+                    </div>
+                    <div>{comment.body}</div>
+                  </div>
+                ))
+              : 'There is not any comments.'}
           </div>
         </div>
       ) : (
